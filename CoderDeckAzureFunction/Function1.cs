96bc14328775f614f8f057c10d54b20d8e3c2e65
DnyaneshwarShivbhakta <shivbhaktadnyaneshwar.1@gmail.com>
2020-04-30 17:21:05 +0530

    Changes for Google Home POC

--- CoderDeckAzureFunction/Function1.cs
+++ CoderDeckAzureFunction/Function1.cs
@@ -9,31 +9,73 @@ using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using CoderDeckServerlessPOC.Models;
 using System.Linq;
+using Google.Protobuf;
+using Google.Cloud.Dialogflow.V2;
+using Newtonsoft.Json.Linq;
+using System.Net.Http;
+using System.Net;
+//using Google.Cloud.Dialogflow.V2;
+//using Google.Protobuf;
 
 namespace CoderDeckAzureFunction
 {
     public static class Function1
     {
         [FunctionName("Function1")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+        public static async Task<HttpResponseMessage> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequestMessage req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
+            JsonParser jsonParser = new JsonParser(JsonParser.Settings.Default.WithIgnoreUnknownFields(true));
+            string name = string.Empty;
+            WebhookRequest request;
+            //using (var reader = new StreamReader(req.Body))
+            //{
+            //StreamReader sr =  new StreamReader(req.Content.ReadAsStreamAsync().Result);
+            //log.LogInformation("My Body{0}", sr);
+            string s = "";
+            MyRequestBody body;
+            var response = req.Content.ReadAsStringAsync().Result;
+            log.LogInformation("Got it" + response);
+            var myParameters= response.ToString().Substring(response.ToString().IndexOf("parameters"));
 
-            string name = req.Query["name"];
+            log.LogInformation("My Parameters {0}", myParameters);
+              myParameters = myParameters.Substring(0, myParameters.IndexOf("}")+1);
+            log.LogInformation("My Exact Parameters {0}", myParameters);
+            myParameters = myParameters.Substring(myParameters.IndexOf("{"));
+            var myfinalResponse = JsonConvert.DeserializeObject<MyRequestBody>(myParameters);
+            log.LogInformation("Final name"+myfinalResponse.name);
+            
+           
             Appointment appointment = null;
             using (CoderDeckPocContext dbContext = new CoderDeckPocContext())
             {
-                appointment = dbContext.Appointment.Where(w => w.Email.Equals(name, StringComparison.InvariantCultureIgnoreCase) && w.AppointmentDate > DateTime.Now).FirstOrDefault();
+                appointment = dbContext.Appointment.Where(w => w.Email.Equals(myfinalResponse.name, StringComparison.InvariantCultureIgnoreCase) && w.AppointmentDate > DateTime.Now).OrderBy(o=>o.AppointmentDate).FirstOrDefault();
             }
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
 
-            return name != null
-                ? (ActionResult)new JsonResult(appointment)
-                : new BadRequestObjectResult(@"Please pass a name on the query string or in the request body");
+            Response responseObject = new Response()
+            {
+                Source = "WebHook",
+                Speech = $"{ appointment.Appointment1} at {appointment.AppointmentDate}",
+                DisplayText = $"{ appointment.Appointment1} at {appointment.AppointmentDate}"
+            };
+
+
+                return req.CreateResponse(HttpStatusCode.OK,responseObject);
+             
         }
     }
+    public class MyRequestBody
+    {
+        public string name { get; set; }
+      
+    }
+    public class Response
+    {
+        public string Speech { get; set; }
+        public string DisplayText { get; set; }
+        public string Source { get; set; }
+    }
 }
+
---
+++ WebHookProject/Class1.cs
@@ -0,0 +1,26 @@
+﻿using Google.Cloud.Dialogflow.V2;
+using Google.Protobuf;
+using System;
+using System.IO;
+
+namespace WebHookProject
+{
+    public class Class1
+    {
+        public string getResponse(StreamReader reader)
+        {
+            JsonParser jsonParser = new JsonParser(JsonParser.Settings.Default.WithIgnoreUnknownFields(true));
+            //    log.LogInformation(req.Query["name"]);
+            WebhookRequest request;
+            //    using (var reader = new StreamReader(req.Body))
+            //    {
+            request = jsonParser.Parse<WebhookRequest>(reader);
+
+            var pas = jsonParser.Parse<WebhookRequest>(reader).QueryResult.Parameters;
+            var na1 = pas.Fields["name"].ToString();
+            //        log.LogInformation("na1{0}", na1);
+            //    }
+            return na1;
+        }
+    }
+}
